6f2e5d349b16584648323479416eff5caa225151
Guglielmo Felici <guglielmo.felici@protonmail.com>
2020-05-18 22:32:37 +0200

    redis

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 
-#[get("/users")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::request::Request;
+use rocket::response::{self, Response, Responder};
+use rocket::http::ContentType;
+
+use rocket_contrib::databases::redis::{self, Commands};
+
+#[database("niccolgur_redis")]
+struct NiccDb(redis::Connection);
+
+struct User {
+    id: String,
+    nickname: String,
+    bio: String,
+}
+
+impl<'r> Responder<'r> for User {
+    fn respond_to(self, _: &Request) -> response::Result<'r> {
+        Response::build()
+            .ok()
+    }
+}
+
+#[get("/user_id")]
+fn user_id(conn: NiccDb) -> Option<String> {
+    match conn.get("user:id") {
+        Ok(id) => Some(id),
+        Err(_) => None,
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![user_id]).launch();
 }
