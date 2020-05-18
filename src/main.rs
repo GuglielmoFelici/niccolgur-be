a4938cf5de9fd8ce4adf6271b9be77b8cc63a847
Guglielmo Felici <guglielmo.felici@protonmail.com>
2020-05-19 01:28:28 +0200

    study default enums and traits

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 
-use rocket::request::Request;
-use rocket::response::{self, Response, Responder};
-use rocket::http::ContentType;
+use std::collections::HashMap;
+use std::ffi::NulError;
+use std::io::Cursor;
 
+use redis::*;
+use rocket::http::ContentType;
+use rocket::request::Request;
+use rocket::response::{self, Responder, Response};
 use rocket_contrib::databases::redis::{self, Commands};
 
 #[database("niccolgur_redis")]
-struct NiccDb(redis::Connection);
+struct NiccDbConn(redis::Connection);
 
 struct User {
     id: String,
@@ -21,18 +25,39 @@ struct User {
 impl<'r> Responder<'r> for User {
     fn respond_to(self, _: &Request) -> response::Result<'r> {
         Response::build()
+            .sized_body(Cursor::new(format!("{}, {}, {}", self.id, self.nickname, self.bio)))
+            .header(ContentType::new("text", "plain"))
             .ok()
     }
 }
 
-#[get("/user_id")]
-fn user_id(conn: NiccDb) -> Option<String> {
-    match conn.get("user:id") {
-        Ok(id) => Some(id),
-        Err(_) => None,
+impl User {
+    fn from_map(map: HashMap<String, String>) -> Option<User> {
+        let nickname = map.get("nickname")?.to_string();
+        let id = map.get(&nickname)?.to_string();
+        let bio = map.get("bio")?.to_string();
+        Option::from(User {
+            nickname,
+            id,
+            bio,
+        })
     }
 }
 
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello cane!"
+}
+
+#[get("/user/<id>")]
+fn user_id(conn: NiccDbConn, id: String) -> Option<User> {
+    let user_map: HashMap<String, String> = match conn.hgetall(format!("user:{}", &id)) {
+        Ok(map) => map,
+        Err(E) => panic!(),
+    };
+    User::from_map(user_map)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![user_id]).launch();
+    rocket::ignite().attach(NiccDbConn::fairing()).mount("/", routes![hello, user_id]).launch();
 }
