18470d40a6ce43917687f4da0999df992b9ac197
Guglielmo Felici <guglielmo.felici@protonmail.com>
2021-01-27 21:09:36 +0100

    istruzioni build

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-$TARGET=armv7-unknown-linux-gnueabihf
+TARGET="armv7-unknown-linux-gnueabihf"
 #$TARGET=aarch64-unknown-linux-gnu
-$COMPILER=arm-linux-gnueabihf-gcc
-rustup target add armv7-unknown-linux-gnueabihf
+COMPILER="arm-linux-gnueabihf-gcc"
+rustup target add armv7-unknown-linux-gnueabihf 1>&/dev/null
 if ! command -v $COMPILER  &> /dev/null; then
     echo "E' necessario installare $COMPILER"
-    exit
+    exit 1
+fi
+if ! grep -q "target.armv7-unknown-linux-gnueabihf" ~/.cargo/config; then
+    echo "E' necessario settare linker = arm-linux-gnueabihf-gcc per il target target.armv7-unknown-linux-gnueabihf in ~/.cargo/config"
+    exit 2
 fi
 
 cargo build --release --target=$TARGET
